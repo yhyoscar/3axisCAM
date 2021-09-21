8da3c20c57b1fe970f6988a3c33c24167ea8103c
Yu <yuhaiyan@88665a3ed2e4.ant.amazon.com>
2021-09-21 11:01:46 -0400

    initial simulator

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, send_from_directory, make_response
+from flask import url_for, jsonify, render_template, request, redirect
+import os
+import pandas as pd 
+import numpy as np
+import copy
+import json 
+import uuid
+import glob 
+import time 
+
+
+#====================================================
+app = Flask(__name__,
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+
+@app.route('/simulator', methods=['GET', 'POST'])
+def home_page():
+
+    return render_template('simulator.html', content = {})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host="0.0.0.0")
+
